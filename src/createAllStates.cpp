4ab68a2a7a7e15d2a83346c2bebda310b4af1942
wenya-r <wwrowe@gmail.com>
2020-09-22 21:36:33 -0400

    complet generation of k states.

--- src/createAllStates.cpp
+++ src/createAllStates.cpp
@@ -1,32 +1,59 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <bits/stdc++.h>
+#include <complex>
 
 using namespace std;
 
 void outputAllStates(int L);
-int outputmStates(int L, int m);
+int outputmStates(int L, int m, vector<string> &vect);
 int bitToNum(string bit);
 string translation(string bit);
 
-void coefficientskMomentum(double coef[][10]);
+double dotProduct(complex<double> * vec1, complex<double> * vec2, int col);
+double dotProduct(complex<double> * vec1, vector<double> * vec2, int col);
+int coefficientskMomentum(complex<double> **arr, int row, int col, int L, int k, vector<string> &vect);
 
 int main()
 {
-    int L = 4;
-    int num;
-    double coeffs[19][10];
-//    double & coeffsRef = coeffs;
-    cout << "row" << sizeof(coeffs)/sizeof(coeffs[0]) << endl;
-    cout << "column" << sizeof(coeffs[0])/sizeof(coeffs[0][0]) << endl;
-    
-    coefficientskMomentum(coeffs);
-    cout << "coeffs : " << coeffs[5][6] << endl;
-//    num = outputmStates(L, 0);
-//   num = bitToNum("0022");
-    cout << " num of states: "<< num << endl;
+    int L = 4, kindex;
+    int num, numStates_m; 
+    double overlap = 0, dot = 0;
+    vector<string> states;
+    numStates_m = outputmStates(L, 0, states);
+
+
+    complex<double> **A = new complex<double>*[numStates_m];
+    for(int i = 0; i < numStates_m; i++)
+    {
+        A[i] = new complex<double>[numStates_m];
+    }
+    for (int i = 0; i < numStates_m; i++)
+    {
+        for (int j = 0; j < numStates_m; j++) {A[i][j] = 0;}
+    }
+
+    vector<double> groundState{-0.07453559924999242, 0.22360679774997777, -0.14907119849998562,
+-0.44721359549995865, 0.2236067977499792, -0.07453559924999299, -0.14907119849998546,
+0.22360679774997771, 0.2236067977499792, -0.2981423969999716, 0.22360679774997913,
+0.2236067977499794, -0.1490711984999861, -0.07453559924999298, 0.22360679774997905,
+-0.44721359549995787, -0.1490711984999861, 0.2236067977499792, -0.07453559924999312};
+
+    kindex = coefficientskMomentum(A, numStates_m, numStates_m, L, 0, states);
+    cout << "kinde: " << kindex << endl;
+    for (int i = 0; i < kindex; i++)
+    {
+        dot = dotProduct(A[i], groundState, numStates_m);
+        overlap = overlap + dot*dot;
+    }
+    cout << "overlap : " << overlap << endl;
+
     return 0;
-}
+} //  end Main
+
+
+
 
 void outputAllStates(int L){
     int numOfStates = pow(3,L);
@@ -47,7 +74,7 @@ void outputAllStates(int L){
     }
 }
 
-int outputmStates(int L, int m){
+int outputmStates(int L, int m, vector<string> &vect){
 // m is Sz total. sum(string)= L+m 
 
     int numOfStates = pow(3,L);
@@ -72,7 +99,8 @@ int outputmStates(int L, int m){
         if (sumBit == L+m)
         {
             total++;
-            cout << state << endl;
+            vect.push_back(state);
+//            cout << state << endl;
         }
     }
     return total;
@@ -98,27 +126,156 @@ int bitToNum(string bit)
 
 string translation(string bit)
 {
-    return "0";
+    string transBit ;
+    transBit = bit[1];
+    for (int i = 2; i < bit.length(); i++)
+    {
+        transBit = transBit+bit[i];
+    }
+    transBit = transBit + bit[0];
+//    cout << " bit is " << bit << endl;
+//    cout << transBit << endl;
+    return transBit;
 
 }
 
-
-void coefficientskMomentum(double coef[][10])
+int bitToIndex(string bit, vector<string> &vect)
 {
-    
-//    cout << "row" << sizeof(coeffs)/sizeof(coeffs[0]) << endl;
-//    cout << "column" << sizeof(coeffs[0])/sizeof(coeffs[0][0]) << endl;
-    int row, col;
-    row = sizeof(coef)/sizeof(coef[0]);
-    col = sizeof(coef[0])/sizeof(coef[0][0]) ;
-    for (int i = 0 ; i < row; i++){
-        for (int j = 0; j < col; j++){
-            coef[i][j] = 8;
+    int index= 0;
+    bool done= false;
+    while(!done)
+    {
+        if (vect[index] == bit)
+        {
+            done = true;
         }
-    }  
+        else{index++;}
+    }
+
+    return index;
+}
+
+
+
+double normalize(complex<double> * vec, int col)
+{
+    double norm = 0;
+    for (int i = 0; i < col; i++)
+    {
+        norm = norm + vec[i].real() * vec[i].real() + vec[i].imag() * vec[i].imag(); 
+    }
+
+    return sqrt(norm);
 
 }
 
+
+double dotProduct(complex<double> * vec1, complex<double> * vec2, int col)
+{
+    complex<double> product = 0;
+    for (int i = 0; i < col; i++)
+    {        product = product + vec1[i]*conj(vec2[i]); }
+    return norm(product);
+}
+
+
+double dotProduct(complex<double> * vec1, vector<double> * vec2, int col)
+{
+    complex<double> product = 0;
+    for (int i = 0; i < col; i++)
+    {        product = product + vec1[i].real()*(vec2[i]) + i*vec1[i].imag()*(vec2[i]); }
+    return norm(product);
+}
+
+
+
+
+
+int coefficientskMomentum(complex<double> **arr, int row, int col, int L, int k, vector<string> &vect)
+{
+    string bit;
+    int i, r, kindex = 0;
+    double km, norm, product ;
+    bool iskState;
+    const double pi = acos(-1);
+    complex<double> * cofArr;
+  
+    //declare complex array and set to zero
+    cofArr = new complex<double>[col];
+    for (int i = 0; i < col; i++) {cofArr[i]=0;}
+    cout << " the number of cofAff = " << sizeof(cofArr) << endl;
+//
+    km = k*2*pi/vect[0].length();
+    
+    for(i = 0; i < row; i++)
+    {
+        cout << i << endl;
+        bit = vect[i];
+        for (int i = 0; i < col; i++) {cofArr[i]=0;}
+        //construct the first state
+        // should be a function find out the coeff for |a> and k 
+        
+        
+        cofArr[bitToIndex(bit, vect)] = 1;        
+        for(r = 1; r < L; r++)
+        {
+            bit = translation(bit);
+           
+            cofArr[bitToIndex(bit, vect)] =  cofArr[bitToIndex(bit, vect)]+exp(i*km*r);        
+        }
+
+//        for(r = 0; r < col ; r++)
+//        {
+//            cout << r << cofArr[r] ;
+//        }
+//        cout << endl;         
+        norm =  normalize(cofArr, col) ;
+
+    // normalize the vector
+        if (norm > 0.00000001)
+        {
+            for (r = 0; r < col; r++)
+            {
+                cofArr[r] = cofArr[r]/norm;
+            }
+            // check orthoganal with all previous
+            iskState = true;
+            for (int j = 0; j <kindex; j++)
+            {
+                if (dotProduct(cofArr, arr[j], col) > 0.5){iskState = false;}
+            }
+            if(iskState)
+            {
+                for (int j=0; j < col; j++)
+                { arr[kindex][j] = cofArr[j];} //  to do
+                kindex++;
+            }
+        
+        }
+    }       
+
+//  print all k states
+    for (r = 0; r < kindex; r++)
+    {
+        for(int j = 0; j < col; j++)
+        { cout << arr[r][j];}
+        cout << endl;
+    }
+
+
+    return kindex;
+
+
+
+
+//    int i, j, k;
+//    for (i = 0 ; i < row; i++){
+//        for (j = 0; j < col; j++){
+//            arr[i][j] = 8+j;
+//        }
+//    }  
+
+}
 
 
 
