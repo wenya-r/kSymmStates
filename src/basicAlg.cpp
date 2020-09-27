7d09e0908e5b733a410f4696f09d34b0502e8295
wenya-r <wwrowe@gmail.com>
2020-09-26 23:26:52 -0400

    clean basicAlg.cpp/hpp

--- src/basicAlg.cpp
+++ src/basicAlg.cpp
@@ -10,6 +10,18 @@
 using namespace std;
 
 
+double normalize(complex<double> * vec, int col)
+{
+    double norm = 0;
+    for (int i = 0; i < col; i++)
+    {
+        norm = norm + vec[i].real() * vec[i].real() + vec[i].imag() * vec[i].imag(); 
+    }
+
+    return sqrt(norm);
+
+}
+
 
 double dotProduct(complex<double> * vec1, complex<double> * vec2, int col)
 {
@@ -27,7 +39,4 @@ double dotProduct(complex<double> * vec1, vector<double> vec2, int col)
     {        product = product + vec1[i].real()*(vec2[i]) + complex<double>(0,1)*vec1[i].imag()*(vec2[i]); }
     return norm(product); //norm : squared magnitude - The norm of (3,4) is 25
 }
-
-
-
 
--- src/basicAlg.hpp
+++ src/basicAlg.hpp
@@ -5,5 +5,6 @@ using namespace std;
 
 
 
+double normalize(complex<double> * vec, int col);
 double dotProduct(complex<double> * vec1, complex<double> * vec2, int col);
 double dotProduct(complex<double> * vec1, vector<double> vec2, int col);
