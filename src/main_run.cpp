8dffa6d5222456691b2d47730313c7a6b0005649
wenya-r <wwrowe@gmail.com>
2020-10-07 22:01:10 -0400

    set up the interface for diff eigenstates files

--- src/main_run.cpp
+++ src/main_run.cpp
@@ -13,43 +13,50 @@ using namespace std;
 
 
 
-int main()
+int main(int argc, char **argv) // L , filename, SzTotal, k
 {
-    int L=22, k = 0;
+    int L, k = 0, SzTotal = 0;
     long long int ni, sz, num, numStates_m, numStates; 
     double overlap = 0, dot = 0, item, projSize, groundNorm;
     vector<string> states;
     cout.precision(17);  
     FILE *fp;
-//    vector<double> groundState;
     vector<unsigned long int> indexTable;
     complex<double> arr;
     complex<double> * groundState;
+    string fileName;
+    string indexName;
 
-
-    numStates_m = outputmStates(L, 0, indexTable);
-    fp = fopen("site22zvo_eigenvec_0_rank_0.dat", "rb");
+//    cout << argv[4] << endl;
+    L = stoi(argv[1]);
+    cout << "L = " << L << endl;
+    fileName = argv[2];
+    cout << "L = " << fileName << endl;
+    SzTotal = stoi(argv[3]);
+    cout << "m = " << SzTotal << endl;
+    k = stoi(argv[4]);
+    fp = fopen(fileName.c_str(), "rb");
         if(fp == NULL){
         printf("file error");
         exit(2);
     }
     cout << "sizeof(ni) " << sizeof(numStates) << endl;
+    cout << "sizeof(arr) " << sizeof(arr) << endl;
     fread(&ni, 4, 1, fp);
-    fread(&numStates, 10, 1, fp);
+    fread(&numStates, 8, 1, fp);
     printf("number of iterations = %d\n",ni );
     printf("number of m_states from file = %d\n",numStates );
     
 //    fp.close();
 
-    groundState = new complex<double>[numStates_m]; // A is the vector for projection
-    fread(&arr, sizeof(arr), 1, fp);
-    for (long int i = 0; i < numStates_m; i++)
+    groundState = new complex<double>[numStates]; // A is the vector for projection
+    fread(&arr, 16, 1, fp);
+    for (int i = 0; i < numStates; i++)
     {  
         fread(&arr, sizeof(arr), 1, fp);
 //        cout << sqrt(norm(arr)) << endl;
         groundState[i] = arr;
-        while (i < 50)
-        {cout << arr << endl;}
+//        if (i > 120 ) {cout << i<< arr << endl;}
     }
     cout << "L is : " << L << endl;
 //    myFile >> numStates_m ;
@@ -63,13 +70,14 @@ int main()
     cout << "finish reading file " << endl;   
 //    myFile.close();
     
+    numStates_m = outputmStates(L, SzTotal, indexTable);
     groundNorm =   dotProduct(groundState, groundState, numStates_m) ;
     cout << "groundNorm = " << groundNorm << endl;
     for (int i =0; i < numStates_m; i++) {groundState[i] = groundState[i]/groundNorm;}
 
-
+    indexName = "indexTableL"+to_string(L)+"m"+to_string(SzTotal);
     // save indexTable
-    saveStates(indexTable, "indexTable20site.dat");
+    saveStates(indexTable, indexName);
     
 //    readStates(indexTable, "indexTable22site.dat");
 
