dc28467f76f15fea220b07da869d1cc367a44c50
wenya-r <wwrowe@gmail.com>
2020-10-10 16:35:45 -0400

    add FileIO.hpp

---
+++ src/FileIO.hpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+void saveStates(vector<unsigned long int> &indexTable, string indexTableName);
+
+
+void readStates(vector<unsigned long int> &indexRead, string indexTableName);
